433ae055f1d4b196c3e06406a031127d514aa176
Jonas Elias Flesch <contato@jonasflesch.com>
2013-03-15 18:23:28 -0400

    Project generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dismassales::Application.config.secret_token = '897a1bdd857540fea18a5e0ab2fdd26820a8126261debf11225fbb8b1d2b7136739bf5981728fe36829687738ab937c3af2557414c6ac1672105ca77ad8c0130'
